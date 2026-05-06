a01066678bf44150819bcefdc98d198c464e630d
Hpm872 <hassan_pena@hotmail.com>
2026-05-05 20:26:42 -0400

    Fix de un bug de la luz que causaba que las decoraciones no sean afectadas por la luz. La adición de la abilidad de prender y apagar la linterna.

--- Assets/Scripts/LampController.cs
+++ Assets/Scripts/LampController.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
 
 public class LampController : MonoBehaviour {
@@ -8,10 +10,38 @@ public class LampController : MonoBehaviour {
     public float baseIntensity = 1f;
     public float flickerAmount = 0.12f;
     public float flickerSpeed = 3f;
+    private InputSystem_Actions inputActions;
+    public bool lampOn = true;
+
+    void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.Flashlight.performed += OnToggle;
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Disable();
+        inputActions.Player.Flashlight.performed -= OnToggle;
+    }
+
+    void OnToggle(InputAction.CallbackContext ctx)
+    {
+        lampOn = !lampOn;
+        if (playerLight != null)
+        {
+            playerLight.enabled = lampOn;
+        }
+    }
 
     void Update()
     {
-        if (playerLight == null) return;
+        if (playerLight == null || !lampOn) return;
         
         float noise = Mathf.PerlinNoise(Time.time * flickerSpeed, 0f);
         playerLight.intensity = baseIntensity + (noise - 0.5f) * flickerAmount;
