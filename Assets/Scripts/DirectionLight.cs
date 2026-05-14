aa4aa5f951278b65aa60d5a0b602093eb4f18d97
Hpm872 <hassan_pena@hotmail.com>
2026-05-13 20:32:57 -0400

    Arreglo de luz direccional

--- Assets/Scripts/DirectionLight.cs
+++ Assets/Scripts/DirectionLight.cs
@@ -1,27 +1,39 @@
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.InputSystem;
 
 public class DirectionLight : MonoBehaviour
 {
     public Light2D spotlight;
     public Transform playerTransform;
-    private float lastDirectionX = 1f;
-    private float lastDirectionY = 1f;
+    private Vector2 lastDirection = Vector2.right;
+    private InputSystem_Actions inputActions;
+
+    void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (spotlight == null || playerTransform == null) return;
 
-        float h = Input.GetAxisRaw("Horizontal");
-        if (h > 0.01f) lastDirectionX = 1f;
-        if (h < -0.01f) lastDirectionX = -1f;
+        Vector2 move = inputActions.Player.Move.ReadValue<Vector2>();
 
-        float v = Input.GetAxisRaw("Vertical");
-        if (v > 0.01f) lastDirectionY = 1f;
-        if (v < -0.01f) lastDirectionY = -1f;
-
-        float targetAngle = (lastDirectionX > 0f)? -90f:90f;
-        spotlight.transform.rotation = Quaternion.Euler(0f, 0f, targetAngle);
+        if (move.sqrMagnitude > 0.01f) lastDirection = move.normalized;
+        
+        float angle = Mathf.Atan2(lastDirection.y, lastDirection.x) * Mathf.Rad2Deg;
+        spotlight.transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
     }
 }
