d42927daabb98f492cdb273acc0bc1b5403bca9e
Hpm872 <hassan_pena@hotmail.com>
2026-06-17 18:08:41 -0400

    Implementación completa del enemigo tipo Chaser.

--- Assets/Scripts/ChaserEnemy.cs
+++ Assets/Scripts/ChaserEnemy.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class ChaserEnemy : MonoBehaviour
@@ -31,8 +32,70 @@ public class ChaserEnemy : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        DetectPlayer();
+
+        if (isChasing) ChasePlayer(); else ReturnToOrigin();
+    }
+
+    private void DetectPlayer()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, detectionRadius, playerLayer);
+        if (hit != null)
+        {
+            playerTarget = hit.transform;
+            isChasing = true;
+        } 
+        else if (isChasing) 
+        {
+            float d = Vector2.Distance(transform.position, playerTarget.position);
+            if (d > loseRadius)
+            {
+                playerTarget = null;
+                isChasing = false;
+            }
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        if (playerTarget == null) return;
+
+        Vector2 dir = ((Vector2)playerTarget.position - (Vector2)transform.position).normalized;
+        rb.linearVelocity = dir * chaseSpeed;
+
+        if (dir.x > 0.01f) sr.flipX = false;
+        if (dir.x < -0.01f) sr.flipX = true;
+    }
+
+    private void ReturnToOrigin()
+    {
+        float dist = Vector2.Distance(transform.position, originPosition);
+        if (dist < 0.01f)
+        {
+            rb.linearVelocity = Vector2.zero;
+            return;
+        }
+
+        Vector2 dir = (originPosition - (Vector2)transform.position).normalized;
+        rb.linearVelocity = dir * returnSpeed;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        var dmg = other.GetComponent<IDamageable>();
+        if (dmg != null) dmg.TakeDamage(damageAmount);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, loseRadius);
     }
 }
