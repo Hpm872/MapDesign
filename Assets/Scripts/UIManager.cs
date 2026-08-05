44d3ca0354f3cd9620e97d735d2d18d4bacd7a2a
Hpm872 <hassan_pena@hotmail.com>
2026-08-04 20:25:49 -0400

    Creación del UI Manager para controlar el UI del juego.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager Instance;
+
+    [Header("Player Healthbar")]
+    public Image playerHealthFill;
+    public int playerMaxHealth = 100;
+
+    [Header("Boss Healthbar")]
+    public Image bossHealthFill;
+    public int bossMaxHealth = 200;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        if (Instance == null) Instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdatePlayerHealth(int currentHealth)
     {
-        
+        if (playerHealthFill != null)
+        {
+            playerHealthFill.fillAmount = (float)currentHealth / playerMaxHealth;
+        }
     }
+
+    public void UpdateBossHealth(int currentHealth)
+    {
+        if (bossHealthFill != null)
+        {
+            bossHealthFill.fillAmount = (float)currentHealth / bossMaxHealth;
+        }
+    }
+
+    /*
+    public void ShowGameOver() 
+    {
+        if (gameOverPanel != null) gameOverPanel.SetActive(true);
+    }
+
+    public void ShowVictory()
+    {
+        if (victoryPanel != null) victoryPanel.SetActive(true);
+    } 
+    */
 }
